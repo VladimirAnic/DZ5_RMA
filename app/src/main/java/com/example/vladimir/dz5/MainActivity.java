33fa576dbb9cef368f7e411878a393ff9d5fcacc
VladimirAnic <vladimiranic@gmail.com>
2017-05-05 19:41:19 +0200

    current location with text added, tracking also added

--- app/src/main/java/com/example/vladimir/dz5/MainActivity.java
+++ app/src/main/java/com/example/vladimir/dz5/MainActivity.java
@@ -1,9 +1,21 @@
 package com.example.vladimir.dz5;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -11,37 +23,198 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends Activity implements OnMapReadyCallback {
 
     GoogleMap mGoogleMap;
     MapFragment mMapFragment;
+    TextView tvLocationDisplay;
     private GoogleMap.OnMapClickListener mCustomOnMapClickListener;
+    private static final int REQUEST_LOCATION_PERMISSION = 10;
+    LocationListener mLocationListener;
+    LocationManager mLocationManager;
+    Marker mCurrLocationMarker = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.initialize();
+        this.mLocationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        this.mLocationListener = new SimpleLocationListener();
     }
 
     private void initialize() {
         this.mMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fGoogleMap);
+        this.tvLocationDisplay = (TextView) this.findViewById(R.id.tvLocationDisplay);
         this.mMapFragment.getMapAsync(this);
         this.mCustomOnMapClickListener = new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
                 MarkerOptions newMarkerOptions = new MarkerOptions();
                 newMarkerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.pin));
-                newMarkerOptions.title("My place");
-                newMarkerOptions.snippet("I declare this my teritory!");
                 newMarkerOptions.position(latLng);
                 mGoogleMap.addMarker(newMarkerOptions);
             }
         };
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (hasLocationPermission() == false) {
+            requestPermission();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (this.hasLocationPermission()) {
+            startTracking();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopTracking();
+    }
+
+    private void startTracking() {
+        Log.d("Tracking", "Tracking started.");
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        String locationProvider = this.mLocationManager.getBestProvider(criteria, true);
+        long minTime = 1000;
+        float minDistance = 10;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        this.mLocationManager.requestLocationUpdates(locationProvider, minTime, minDistance,
+                this.mLocationListener);
+    }
+
+    private void stopTracking() {
+        Log.d("Tracking", "Tracking stopped.");
+        this.mLocationManager.removeUpdates(this.mLocationListener);
+    }
+    private void updateLocationDisplay(Location location){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Lat: ").append(location.getLatitude()).append("\n");
+        stringBuilder.append("Lon: ").append(location.getLongitude()).append("\n");
+        tvLocationDisplay.setText(stringBuilder.toString());
+
+        if(Geocoder.isPresent()){
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            try {
+                List<Address> nearByAddresses = geocoder.getFromLocation(
+                        location.getLatitude(), location.getLongitude(),1);
+                if(nearByAddresses.size() > 0) {
+                    stringBuilder = new StringBuilder();
+                    Address nearestAddress = nearByAddresses.get(0);
+                    stringBuilder.append(nearestAddress.getAddressLine(0)).append("\n")
+                            .append(nearestAddress.getLocality()).append("\n")
+                            .append(nearestAddress.getCountryName());
+                    tvLocationDisplay.append(stringBuilder.toString());
+                }
+            } catch (IOException e) { e.printStackTrace(); }
+        }
+        if (mCurrLocationMarker != null) {
+            mCurrLocationMarker.remove();
+        }
+        MarkerOptions newMarkerOptions = new MarkerOptions();
+        newMarkerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.current_location));
+        newMarkerOptions.position(new LatLng(location.getLatitude(), location.getLongitude()));
+        newMarkerOptions.title("Here I am");
+        newMarkerOptions.snippet("Wow! I'm finally aware of my location!");
+        mCurrLocationMarker = mGoogleMap.addMarker(newMarkerOptions);
+        mCurrLocationMarker.showInfoWindow();
+    }
+
+    private boolean hasLocationPermission() {
+        String LocationPermission = android.Manifest.permission.ACCESS_FINE_LOCATION;
+        int status = ContextCompat.checkSelfPermission(this, LocationPermission);
+        if (status == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+    private void requestPermission(){
+        String[] permissions = new String[]{ android.Manifest.permission.ACCESS_FINE_LOCATION};
+        ActivityCompat.requestPermissions(MainActivity.this,
+                permissions, REQUEST_LOCATION_PERMISSION);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_LOCATION_PERMISSION:
+                if(grantResults.length >0){
+                    if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                        Log.d("Permission","Permission granted. User pressed allow.");
+                    }
+                    else{
+                        Log.d("Permission","Permission not granted. User pressed deny.");
+                        askForPermission();
+                    }
+                }
+        }
+    }
+    private void askForPermission(){
+        boolean shouldExplain = ActivityCompat.shouldShowRequestPermissionRationale(
+                MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if(shouldExplain){
+            Log.d("Permission","Permission should be explained, - don't show again not clicked.");
+            this.displayDialog();
+        }
+        else{
+            Log.d("Permission","Permission not granted. User pressed deny and don't show again.");
+            tvLocationDisplay.setText("Sorry, we really need that permission");
+        }
+    }
+    private void displayDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Location permission")
+                .setMessage("We display your location and need your permission")
+                .setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("Permission", "User declined and won't be asked again.");
+                        dialog.cancel();
+                    }
+                })
+                .setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("Permission","Permission requested because of the explanation.");
+                        requestPermission();
+                        dialog.cancel();
+                    }
+                })
+                .show();
+    }
+
+    private class SimpleLocationListener implements LocationListener{
+        @Override
+        public void onLocationChanged(Location location) { updateLocationDisplay(location); }
+        @Override public void onStatusChanged(String provider, int status, Bundle extras) {}
+        @Override public void onProviderEnabled(String provider) { }
+        @Override public void onProviderDisabled(String provider) {}
+    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
@@ -65,4 +238,5 @@ public class MainActivity extends Activity implements OnMapReadyCallback {
         this.mGoogleMap.setMyLocationEnabled(true);
 
     }
+
 }
